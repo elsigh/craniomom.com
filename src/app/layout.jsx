924045ccbecf9ec699cd32d10a3cd55813ac766e
elsigh <elsigh@gmail.com>
2023-09-14 09:13:12 -0700

    adds @vercel/analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
-import { RootLayout } from '@/components/RootLayout'
+import { RootLayout } from '@/components/RootLayout';
+import { Analytics } from '@vercel/analytics/react';
 
-import '@/styles/tailwind.css'
+import '@/styles/tailwind.css';
 
 export const metadata = {
   metadataBase: new URL('https://www.craniomom.com'),
@@ -22,6 +23,7 @@ export default function Layout({ children }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <Analytics/>
       </body>
     </html>
   )
